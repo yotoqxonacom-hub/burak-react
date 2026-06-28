d4e0d10019f36f7a53c33e208fad0ec840d349f7
METHEW <yotoqxona.com@gmail.com>
2026-06-28 13:41:39 +0900

    fix: develop screen based components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,49 @@
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { HomePage } from "../screens/homePage";
+import { ProductsPage } from "../screens/productsPage";
+import { OrdersPage } from "../screens/ordersPage";
+import { UsersPage } from "../screens/userPage";
+
 function App() {
-  return <div>Test</div>;
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">HomePage</Link>
+            </li>
+            <li>
+              <Link to="/products">ProductsPage</Link>
+            </li>
+            <li>
+              <Link to="/orders">OrdersPage</Link>
+            </li>
+            <li>
+              <Link to="/member-page">UserPage</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/products">
+            <ProductsPage />
+          </Route>
+          <Route path="/orders">
+            <OrdersPage />
+          </Route>
+          <Route path="/member-page">
+            <UsersPage />
+          </Route>
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
+
 
 export default App;
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,28 @@
+
 import { configureStore } from "@reduxjs/toolkit";
+import type { ThunkAction, Action } from "@reduxjs/toolkit";
+import { HomePage } from "../screens/homePage/index";
+import { ProductsPage } from "../screens/productsPage/index";
+import { OrdersPage } from "../screens/ordersPage/index";
+import reduxLogger from "redux-logger";
 
 export const store = configureStore({
-    reducer: {},
+  middleware: (getDefaultMiddleware) =>
+    // @ts-ignore
+    getDefaultMiddleware().concat(reduxLogger),
+  reducer: {
+    homePage: HomePage,
+    productsPage: ProductsPage,
+    ordersPage: OrdersPage,
+  },
 });
 
-export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
+

28527d07bf414031b00a95cb1401195d4c411cd9
METHEW <yotoqxona.com@gmail.com>
2026-06-28 18:23:34 +0900

    feat: develop header and footer part one

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,31 +7,42 @@ import { UsersPage } from "../screens/userPage";
 import { HomeNavbar } from "./components/headers/HomeNavbar";
 import { OtherNavbar } from "./components/headers/OtherNavbar";
 import { Footer } from "./components/footer";
+import "../css/App.css";
+import "../css/navbar.css";
+import { HelpPage } from "../screens/helpPage";
 
-function App() {
+function Layout() {
   const location = useLocation();
-  console.log("location:", location);
 
   return (
+    <>
+      {location.pathname === "/" ? <HomeNavbar /> : <OtherNavbar />}
+      <Switch>
+        <Route path="/products">
+          <ProductsPage />
+        </Route>
+        <Route path="/orders">
+          <OrdersPage />
+        </Route>
+        <Route path="/member-page">
+          <UsersPage />
+        </Route>
+        <Route path="/help">
+          <HelpPage />
+        </Route>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+      </Switch>
+      <Footer />
+    </>
+  );
+}
+
+function App() {
+  return (
     <Router>
-      <>
-        {location.pathname === "/" ? <HomeNavbar /> : <OtherNavbar />}
-        <Switch>
-          <Route path="/products">
-            <ProductsPage />
-          </Route>
-          <Route path="/orders">
-            <OrdersPage />
-          </Route>
-          <Route path="/member-page">
-            <UsersPage />
-          </Route>
-          <Route path="/">
-            <HomePage />
-          </Route>
-        </Switch>
-        <Footer />
-      </>
+      <Layout />
     </Router>
   );
 }

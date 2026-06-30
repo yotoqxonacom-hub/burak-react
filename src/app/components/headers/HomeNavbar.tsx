80ab31c255c69188e04034a38debd276c8125e3e
METHEW <yotoqxona.com@gmail.com>
2026-06-30 19:10:10 +0900

    feat publish otherNavbar header components

--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -3,66 +3,67 @@ import { NavLink } from "react-router-dom";
 
 export function HomeNavbar() {
     const authMember = null;
-    return (<div className="home-navbar">
-        <Container className="navbar-container">
-            <Stack className="menu">
-                <Box>
-                    <NavLink to={"/"}>
-                        <img className="brand-logo" src="/icons/burak.svg" />
-                    </NavLink>
-                </Box>
-                <Stack className="links" >
-                    <Box className={"hover-line"}>
-                        <NavLink to={"/"} activeClassName={"underline"}>
-                            Home
+    return (
+        <div className="home-navbar">
+            <Container className="navbar-container">
+                <Stack className="menu">
+                    <Box>
+                        <NavLink to={"/"}>
+                            <img className="brand-logo" src="/icons/burak.svg" />
                         </NavLink>
                     </Box>
-                    <Box className={"hover-line"}>
-                        <NavLink to={"/products"} activeClassName={"underline"}>
-                            Products
-                        </NavLink>
-                    </Box>
-                    <Box className={"hover-line"}>
-                        {authMember ? (<Box className={"hover-line"}>
-                            <NavLink to={"/orders"} activeClassName={"underline"}>
-                                Orders
+                    <Stack className="links" >
+                        <Box className={"hover-line"}>
+                            <NavLink to={"/"} activeClassName={"underline"}>
+                                Home
                             </NavLink>
                         </Box>
-                        ) : null}
-                    </Box>
-                    <Box className={"hover-line"}>
-                        {authMember ? (<Box className={"hover-line"}>
-                            <NavLink to={"/member-page"} activeClassName={"underline"}>
-                                My Page
+                        <Box className={"hover-line"}>
+                            <NavLink to={"/products"} activeClassName={"underline"}>
+                                Products
                             </NavLink>
                         </Box>
-                        ) : null}
-                    </Box>
-                    <Box className={"hover-line"}>
-                        <NavLink to={"/help"} activeClassName={"underline"}>
-                            Help
-                        </NavLink>
-                    </Box>
-                    {/* Basket */}
+                        <Box className={"hover-line"}>
+                            {authMember ? (<Box className={"hover-line"}>
+                                <NavLink to={"/orders"} activeClassName={"underline"}>
+                                    Orders
+                                </NavLink>
+                            </Box>
+                            ) : null}
+                        </Box>
+                        <Box className={"hover-line"}>
+                            {authMember ? (<Box className={"hover-line"}>
+                                <NavLink to={"/member-page"} activeClassName={"underline"}>
+                                    My Page
+                                </NavLink>
+                            </Box>
+                            ) : null}
+                        </Box>
+                        <Box className={"hover-line"}>
+                            <NavLink to={"/help"} activeClassName={"underline"}>
+                                Help
+                            </NavLink>
+                        </Box>
+                        {/* Basket */}
 
-                    {!authMember ? (<Box><Button variant="contained" className="login-button">LOGIN</Button></Box>) : (<img className="user-avatar" src={"/icons/default-user.svg"} aria-haspopup={"true"} />)}
+                        {!authMember ? (<Box><Button variant="contained" className="login-button">LOGIN</Button></Box>) : (<img className="user-avatar" src={"/icons/default-user.svg"} aria-haspopup={"true"} />)}
+                    </Stack>
                 </Stack>
-            </Stack>
-            <Stack className={"header-frame"}>
-                <Stack className={"detail"}>
-                    <Box className={"head-main-txt"} >World's Most Delicious Cousine</Box>
-                    <Box className={"wel-txt"}>The Choice not just a choice</Box>
-                    <Box className={"service-txt"}>24 hours service</Box>
-                    <Box className={"signup"}>
-                        {!authMember ? <Button variant={"contained"} className={"signup-button"} > SIGN UP</Button> : null}
+                <Stack className={"header-frame"}>
+                    <Stack className={"detail"}>
+                        <Box className={"head-main-txt"} >World's Most Delicious Cousine</Box>
+                        <Box className={"wel-txt"}>The Choice not just a choice</Box>
+                        <Box className={"service-txt"}>24 hours service</Box>
+                        <Box className={"signup"}>
+                            {!authMember ? <Button variant={"contained"} className={"signup-button"} > SIGN UP</Button> : null}
 
+                        </Box>
+                    </Stack>
+                    <Box className={"logo-frame"}>
+                        <div className={"logo-img"}></div>
                     </Box>
                 </Stack>
-                <Box className={"logo-frame"}>
-                    <div className={"logo-img"}></div>
-                </Box>
-            </Stack>
-        </Container>
-    </div>
+            </Container>
+        </div>
     );
 }

91fe6514e93fb28e11070daeb05b90743b145899
METHEW <yotoqxona.com@gmail.com>
2026-07-22 09:22:37 +0500

    feat config & env variables

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -5,15 +5,13 @@ import Events from "./Events";
 import NewDishes from "./NewDishes";
 import PopularDishes from "./PopularDishes";
 import Statistics from "./Statistics";
-import "../../../css/home.css";
-
-
 import { useDispatch, useSelector } from "react-redux";
 import { Dispatch } from "@reduxjs/toolkit";
 import { createSelector } from "reselect";
 import { setPopularDishes } from "./slice";
 import { retrievePopularDishes } from "./selector";
 import { Product } from "../../../lib/types/product";
+import "../../../css/home.css";
 
 /** REDUX SLICE & SELECTOR **/
 const actionDispatch = (dispatch: Dispatch) => ({
@@ -26,50 +24,8 @@ export default function HomePage() {
   const { popularDishes } = useSelector(popularDishesRetriever);
   // Selector: Store => Data
 
-  useEffect(() => {
-    // Backend server data request => Data 
-    const result = [
-      {
-        "_id": "6a3b766036199f75697332d2",
-        "productStatus": "PROCESS",
-        "productCollection": "DISH",
-        "productName": "KEBAB",
-        "productPrice": 13,
-        "productLeftCount": 121,
-        "productSize": "NORMAL",
-        "productVolume": 1,
-        "productDesc": "tasty",
-        "productImages": [],
-        "productViews": 0,
-        "createdAt": "2026-06-24T06:17:04.380Z",
-        "updatedAt": "2026-06-24T06:17:04.380Z",
-        "__v": 0
-      },
-      {
-        "_id": "6a3b6b7696366dc7169ba341",
-        "productStatus": "PROCESS",
-        "productCollection": "DISH",
-        "productName": "KEBAB",
-        "productPrice": 18,
-        "productLeftCount": 80,
-        "productSize": "LARGE",
-        "productVolume": 1,
-        "productDesc": "bigger than normal and very delicious",
-        "productImages": [],
-        "productViews": 0,
-        "createdAt": "2026-06-24T05:30:30.876Z",
-        "updatedAt": "2026-06-24T05:30:30.876Z",
-        "__v": 0
-      },
 
-    ]
-
-
-    // Slice : Data => Store
-    //@ts-ignore
-    setPopularDishes(result);
-  }, []);
-
+  useEffect(() => { }, []);
 
   return (
     <div className={"homepage"}>
---
+++ src/lib/config.ts
@@ -0,0 +1,11 @@
+import { error } from "console";
+
+export const serverApi: string = `$(process.env.REACT_APP_API_URL)`;
+
+export const Message = {
+    error1: "Something went wrong!",
+    error2: "Please login first!",
+    error3: "Please fulfill all inputs!",
+    error4: " Message is empty ",
+    error5: "Only images with jpeg , jpg, png format allowed",
+}

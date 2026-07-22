6d2d518d405fbbf92dd7c62ba0e2fce52c972f52
METHEW <yotoqxona.com@gmail.com>
2026-07-22 13:19:23 +0500

    fix: retreive fresh menu and modify newDishes components

--- src/app/screens/homePage/PopularDishes.tsx
+++ src/app/screens/homePage/PopularDishes.tsx
@@ -29,10 +29,10 @@ export default function PopularDishes() {
           <Box className="category-title">Popular Dishes</Box>
           <Stack className="cards-frame">
             {popularDishes.length !== 0 ? (
-              popularDishes.map((ele: Product) => {
-                const imagePath = `${serverApi}/${ele.productImages[0]}`;
+              popularDishes.map((product: Product) => {
+                const imagePath = `${serverApi}/${product.productImages[0]}`;
                 return (
-                  <CssVarsProvider key={String(ele._id)}>
+                  <CssVarsProvider key={String(product._id)}>
                     <Card className={"card"}>
                       <CardCover>
                         <img src={imagePath} alt="" />
@@ -49,7 +49,7 @@ export default function PopularDishes() {
                             textColor="#fff"
                             mb={1}
                           >
-                            {ele.productName}
+                            {product.productName}
                           </Typography>
                           <Typography
                             sx={{
@@ -59,7 +59,7 @@ export default function PopularDishes() {
                               display: "flex",
                             }}
                           >
-                            {ele.productViews}
+                            {product.productViews}
                             <VisibilityIcon
                               sx={{ fontSize: 25, marginLeft: "5px" }}
                             />
@@ -80,7 +80,7 @@ export default function PopularDishes() {
                           startDecorator={<DescriptionOutlinedIcon />}
                           textColor="neutral.300"
                         >
-                          {ele.productDesc}
+                          {product.productDesc}
                         </Typography>
                       </CardOverflow>
                     </Card>
--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -8,7 +8,7 @@ import Statistics from "./Statistics";
 import { useDispatch, useSelector } from "react-redux";
 import { Dispatch } from "@reduxjs/toolkit";
 import { createSelector } from "reselect";
-import { setPopularDishes } from "./slice";
+import { setNewDishes, setPopularDishes } from "./slice";
 import { retrievePopularDishes } from "./selector";
 import { Product } from "../../../lib/types/product";
 import "../../../css/home.css";
@@ -18,11 +18,12 @@ import { ProductCollection } from "../../../lib/enums/product.enum";
 /** REDUX SLICE & SELECTOR **/
 const actionDispatch = (dispatch: Dispatch) => ({
   setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
+  setNewDishes: (data: Product[]) => dispatch(setNewDishes(data)),
 });
 
 
 export default function HomePage() {
-  const { setPopularDishes } = actionDispatch(useDispatch());
+  const { setPopularDishes, setNewDishes } = actionDispatch(useDispatch());
 
   useEffect(() => {
     // Backend service data fetch => Data
@@ -38,6 +39,19 @@ export default function HomePage() {
     }).catch((err) => {
       console.log(err)
     });
+
+
+    product.getProducts({
+      page: 1,
+      limit: 4,
+      order: "createdAt",
+      //productCollection: ProductCollection.DISH,
+    }).then((data) => {
+      setNewDishes(data);
+    }).catch((err) => {
+      console.log(err)
+    });
+
   }, []);
 
   return (
